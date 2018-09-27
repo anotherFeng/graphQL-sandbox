ed3cccfd961480d6177255385745c8c232e6255b
anotherFeng <regnagleppod@gmail.com>
2018-09-26 21:14:17 -0400

    Add apollo graphql by example files

---
+++ apollo-server-job-board/client/src/Requests.js
@@ -0,0 +1,79 @@
+const endpointURL = 'http://localhost:9000/graphql';
+
+async function graphqlRequest(query, variables){
+  const response = await fetch(endpointURL,{
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: query,
+      variables: variables
+    })
+  });
+  const responseBody = await response.json();
+  if(responseBody.errors){
+    const message = responseBody.errors.map(error => error.message).join('\n');
+    throw new Error(message)
+  }
+  return responseBody.data;
+}
+
+export async function loadCompany(id){
+  const query = `query CompanyQuery($id: ID!){
+    company(id: $id){
+      name
+      description
+      jobs {
+        id
+        title
+      }
+    }
+  }`;
+  const data = await graphqlRequest(query, {id});
+  return data.company;
+}
+
+export async function createJob(input){
+  const mutation = `mutation CreateJob($input: CreateJobInput){
+    job: createJob(input: $input){
+      id
+      title
+      company{
+        id
+        name
+      }
+    }
+  }`
+  const {job} = await graphqlRequest(mutation, {input});
+  return job;
+}
+
+export async function loadJob(id){
+  const query = `query JobQuery($id: ID!){
+    job(id: $id){
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }`;
+  const data = await graphqlRequest(query, {id})
+  return data.job;
+}
+
+export async function loadJobs(){
+  const query = `{
+    jobs {
+      id
+      title
+      company{
+        id
+        name
+      }
+    }
+  }`;
+  const data = await graphqlRequest(query);
+  return data.jobs;
+}
---
+++ apollo-server-job-board/server/resolvers.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+const Query = {
+  company: (root, args) => db.companies.get(args.id),
+  job: (root, args) => db.jobs.get(args.id), 
+  jobs: () => db.jobs.list()
+}
+
+const Mutation = {
+  createJob: (root, {input}, context) =>{
+    //check user auth
+    if(!context.user){
+      throw new Error("unathorized");
+    }
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  }
+}
+
+const Company = {
+  jobs: (company) => db.jobs.list()
+    .filter((job)=> job.companyId === company.id)
+}
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId)
+}
+
+module.exports = {Query, Mutation, Company, Job};
