7a024c8c1fb3c066d05cac2328bcbceb0572daf4
anotherFeng <regnagleppod@gmail.com>
2018-09-27 00:50:12 -0400

    Refactor requests to all use apollo client

--- apollo-server-job-board/client/src/Requests.js
+++ apollo-server-job-board/client/src/Requests.js
@@ -1,37 +1,30 @@
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost';
+import gql from 'graphql-tag';
 import { getAccessToken, isLoggedIn } from './auth.js';
 
 const endpointURL = 'http://localhost:9000/graphql';
+const authLink = new ApolloLink((operation, forward) => {
+  if(isLoggedIn()) {
+    operation.setContext({
+      headers: {
+        'authorization': 'Bearer ' + getAccessToken()
+      }
+    })
+  }
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({uri: endpointURL}),
+  link: ApolloLink.from([
+    authLink,
+    new HttpLink({uri: endpointURL}),
+  ]),
   cache: new InMemoryCache()
 });
 
-async function graphqlRequest(query, variables){
-  const request = {
-    method: 'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: query,
-      variables: variables
-    })
-  };
-  if(isLoggedIn()) {
-    request.headers['authorization'] = 'Bearer ' + getAccessToken();
-  }
-
-  const response = await fetch(endpointURL, request);
-  const responseBody = await response.json();
-  if(responseBody.errors){
-    const message = responseBody.errors.map(error => error.message).join('\n');
-    throw new Error(message)
-  }
-  return responseBody.data;
-}
-
 export async function loadCompany(id){
-  const query = `query CompanyQuery($id: ID!){
+  const query = gql`
+  query CompanyQuery($id: ID!){
     company(id: $id){
       name
       description
@@ -41,27 +34,29 @@ export async function loadCompany(id){
       }
     }
   }`;
-  const data = await graphqlRequest(query, {id});
+  const {data} = await client.query({query, variables: {id}});
   return data.company;
-}
+};
 
 export async function createJob(input){
-  const mutation = `mutation CreateJob($input: CreateJobInput){
-    job: createJob(input: $input){
-      id
-      title
-      company{
+  const mutation = gql`
+    mutation CreateJob($input: CreateJobInput){
+      job: createJob(input: $input){
         id
-        name
+        title
+        company{
+          id
+          name
+        }
       }
-    }
-  }`
-  const {job} = await graphqlRequest(mutation, {input});
-  return job;
-}
+    }`
+  const {data} = await client.mutate({mutation, variables: {input}});
+  return data.job;
+};
 
 export async function loadJob(id){
-  const query = `query JobQuery($id: ID!){
+  const query = gql`
+    query JobQuery($id: ID!){
     job(id: $id){
       id
       title
@@ -72,12 +67,12 @@ export async function loadJob(id){
       description
     }
   }`;
-  const data = await graphqlRequest(query, {id})
+  const {data} = await client.query({query, variables: {id}})
   return data.job;
 }
 
 export async function loadJobs(){
-  const query = `{
+  const query = gql`{
     jobs {
       id
       title
@@ -87,6 +82,28 @@ export async function loadJobs(){
       }
     }
   }`;
-  const data = await graphqlRequest(query);
+  const {data} = await client.query({query});
   return data.jobs;
 }
+
+// async function graphqlRequest(query, variables){
+//   const request = {
+//     method: 'POST',
+//     headers: {'content-type': 'application/json'},
+//     body: JSON.stringify({
+//       query: query,
+//       variables: variables
+//     })
+//   };
+//   if(isLoggedIn()) {
+//     request.headers['authorization'] = 'Bearer ' + getAccessToken();
+//   }
+
+//   const response = await fetch(endpointURL, request);
+//   const responseBody = await response.json();
+//   if(responseBody.errors){
+//     const message = responseBody.errors.map(error => error.message).join('\n');
+//     throw new Error(message)
+//   }
+//   return responseBody.data;
+// }
